90297324845e4ed402264ca627ca0c3e98b8b2e0
Keir Hewitt <keir11@hotmail.com>
2022-10-22 16:05:41 +0100

    Style of main form setup with resource dictionary.

---
+++ NapierBankMessenger/Commands/RelayCommand.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace NapierBankMessenger.Commands
+{
+    internal class RelayCommand : ICommand
+    {
+        private Action<object> _execute;
+        private Func<object, bool> _canExecute;
+
+        public RelayCommand(Action<object> execute, Func<object, bool> canExecute = null)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value;  }
+        }
+
+        public bool CanExecute(object param)
+        {
+            return _canExecute == null || _canExecute(param);
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute(parameter);
+        }
+    }
+}
---
+++ NapierBankMessenger/MVVM/ViewModel/BaseViewModel.cs
@@ -0,0 +1,16 @@
+﻿
+using System.ComponentModel;
+
+
+namespace NapierBankMessenger.MVVM.ViewModel
+{
+    internal class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        
+        public void OnChanged(string val)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(val));
+        }
+    }
+}
