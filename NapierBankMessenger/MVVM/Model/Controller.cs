18eeb388e01b1363be9cfe84c804f6fe426dadde
Keir Hewitt <keir11@hotmail.com>
2022-10-27 20:25:29 +0100

    Successfully binded HomePage to MainWIndow.xaml

---
+++ NapierBankMessenger/MVVM/Model/Controller.cs
@@ -0,0 +1,45 @@
+﻿
+using System.Collections.Generic;
+
+namespace NapierBankMessenger.MVVM.Model
+{
+    public class Controller
+    {
+        private List<Message> _messages { get; set; }
+        private List<SIR> _SIRs { get; set; }
+        public Message[] Messages { get; set; }
+
+        public void validateMessage(Message message)
+        {
+
+        }
+
+        public void sanitizeMessage(Message message)
+        {
+
+        }
+
+        public void categorizeMessage(Message message)
+        {
+
+        }
+
+        public void sendMessageToJSON(Message message)
+        {
+
+        }
+
+        public void loadMessagesFromJSON()
+        {
+
+        }
+
+        public void addMessage(Message message) {  _messages.Add(message); }
+        public void removeMessage(Message message) { _messages.Remove(message); }
+        public void addSIR(SIR sir) { _SIRs.Add(sir); }
+        public void removeSIR(SIR sir) { _SIRs.Remove(sir); }
+
+        public List<Message> getMessages() { return _messages; }
+        public List<SIR> getSIRs() { return _SIRs; }
+    }
+}
--- NapierBankMessenger/MVVM/View/HomePage.xaml.cs
+++ NapierBankMessenger/MVVM/View/HomePage.xaml.cs
@@ -1,18 +1,17 @@
 ﻿using System;
 using NapierBankMessenger.MVVM.Model;
 using System.Windows.Controls;
-using Caliburn.Micro;
 
 namespace NapierBankMessenger.MVVM.View
 {
 
     public partial class HomePage : UserControl
     {
-        public BindableCollection<User> users { get; set; }
 
         public HomePage()
         {
             InitializeComponent();
+            DataContext = new Controller();
         }
     }
 }
