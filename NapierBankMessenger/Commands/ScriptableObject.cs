d5c99aa98e58cc857b1a6de6fff49abfb57af425
Keir Hewitt <keir11@hotmail.com>
2022-10-27 20:24:29 +0100

    Made scriptable object class so that all ViewModel's can inherit from it.

---
+++ NapierBankMessenger/Commands/ScriptableObject.cs
@@ -0,0 +1,16 @@
+﻿
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace NapierBankMessenger.MVVM.ViewModel
+{
+    public class ScriptableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        
+        public void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
