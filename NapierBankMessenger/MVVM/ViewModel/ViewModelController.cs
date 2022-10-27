5ffc34dbbdd90eee5c8af6f74c6bfee9c3090b23
Keir Hewitt <keir11@hotmail.com>
2022-10-27 20:25:08 +0100

    Renamed and re-tooled HomeViewModel to ViewModelController - will now select the current view.

---
+++ NapierBankMessenger/MVVM/ViewModel/ViewModelController.cs
@@ -0,0 +1,40 @@
+﻿
+using NapierBankMessenger.MVVM.View;
+using NapierBankMessenger.Commands;
+
+namespace NapierBankMessenger.MVVM.ViewModel
+{
+    public class ViewModelController : ScriptableObject
+    {
+        public HomePage homePage { get; }
+
+        private object _currentView;
+        private object _messageListView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set 
+            { 
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public object MessageView
+        {
+            get { return _messageListView; }
+            set
+            {
+                _messageListView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ViewModelController()
+        {
+            homePage = new HomePage();
+            MessageView = homePage;
+        }
+    }
+}
