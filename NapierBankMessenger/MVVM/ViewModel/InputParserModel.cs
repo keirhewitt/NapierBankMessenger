8d9085ed0171b398eee8c7cf40f4dc65e32ba5ef
Keir Hewitt <keir11@hotmail.com>
2022-10-28 15:02:59 +0100

    Created new view and VM for Input Parsing.

---
+++ NapierBankMessenger/MVVM/ViewModel/InputParserModel.cs
@@ -0,0 +1,27 @@
+﻿
+using NapierBankMessenger.MVVM.View;
+
+namespace NapierBankMessenger.MVVM.ViewModel
+{
+    public class InputParserModel : ScriptableObject
+    {
+        public InputParser InputParserView { get; }
+        public object _ipView;
+
+        public object View
+        {
+            get { return _ipView; }
+            set
+            {
+                _ipView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public InputParserModel()
+        {
+            InputParserView = new InputParser();
+            View = _ipView;
+        }
+    }
+}
