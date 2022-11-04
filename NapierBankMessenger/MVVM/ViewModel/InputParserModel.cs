49a7440ddf7c9029a0b3a792dca7098f659212ca
Keir Hewitt <keir11@hotmail.com>
2022-11-04 14:52:44 +0000

    Subject field disable functionality working as expected.

--- NapierBankMessenger/MVVM/ViewModel/InputParserModel.cs
+++ NapierBankMessenger/MVVM/ViewModel/InputParserModel.cs
@@ -1,31 +1,83 @@
 ﻿
-using NapierBankMessenger.Commands;
-using NapierBankMessenger.MVVM.View;
-using System.Diagnostics;
-using System.Windows.Input;
+using System.Windows.Data;
 
 namespace NapierBankMessenger.MVVM.ViewModel
 {
-    public class InputParserModel : ViewModelController
+    public class InputParserModel : ScriptableObject
     {
-        public InputParser InputParserView { get; }
-        public object _ipView;
-       
+        private readonly string[] email_format_sequences = { "@hotmail.com", "@gmail.com", "@live.napier.ac.uk" };
 
-        public object View
+        private string _sender;
+        private string _subject;
+        private string _body;
+
+        public bool subjectRequired = true;
+
+        // Binded to Subject Text Box - Field linked with "IsEnabled"
+        public bool SubjectLineDisabled
         {
-            get { return _ipView; }
+            get { return subjectRequired; }
+        }
+
+        public string Sender 
+        { 
+            get { return _sender; }
             set
             {
-                _ipView = value;
+                _sender = value;
+                // Sender line is in Email format, enable Subject Box
+                if (CheckForEmail(_sender))
+                {
+                    subjectRequired = true;
+                }
+                else
+                {
+                    subjectRequired = false;
+                }
+                OnPropertyChanged();
+                OnPropertyChanged("SubjectLineDisabled");
+            } 
+        }
+
+        public string Subject
+        {
+            get { return _subject; }
+            set
+            {
+                _subject = value;
                 OnPropertyChanged();
             }
         }
 
+        public string Body
+        {
+            get { return _body; }
+            set
+            {
+                _body = value;
+                OnPropertyChanged();
+            }
+        }
+
+
         public InputParserModel()
         {
-            InputParserView = new InputParser();
-            View = _ipView;
+            
+        }
+
+        private void OnTargetUpdate(object sender, DataTransferEventArgs args)
+        {
+
+        }
+
+        public bool CheckForEmail(string sender_line)
+        {
+            for (int i = 0; i < email_format_sequences.Length; i++)
+            {
+                if (sender_line.Contains(email_format_sequences[i]))
+                    return true;
+            }
+            return false;
         }
 
     }
