880ebb226a2af7b3d6a7b907f3ff469798ca84fe
Keir Hewitt <keir11@hotmail.com>
2022-11-16 13:18:29 +0000

    No permanent changes.

--- NapierBankMessenger/MVVM/FileIO/Textspeak.cs
+++ NapierBankMessenger/MVVM/FileIO/Textspeak.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
@@ -7,23 +8,20 @@ namespace NapierBankMessenger.MVVM.FileIO
     // Handles loading of Textspeak abbreviations CSV file
     public static class Textspeak
     {
+        // Separate lists for abbreviations and phrases
         private static List<string> abbreviations = new List<string>();
         private static List<string> phrases = new List<string>();
 
         // Read data from csv file and append each column to lists
         public static void IO()
         {
-            var result = File.ReadAllLines("C:\\Users\\Keir\\OneDrive\\University Work\\Year 3\\Software Engineering\\Coursework\\textwords.csv")
-                .Select(row=>row.Split(';')).ToList();
-
-            foreach (string abb in result[0])
+            string[] l = File.ReadAllLines(@"C:\\Users\\Keir\\OneDrive\\University Work\\Year 3\\Software Engineering\\Coursework\\textwords.csv");
+            foreach (string s in l)
             {
-                abbreviations.Add(abb);
-            }
-
-            foreach (string phr in result[1])
-            {
-                phrases.Add(phr);
+                string abbrev = s.Split(',')[0];
+                string phrase = s.Split(',')[1];
+                abbreviations.Add(abbrev);
+                phrases.Add(phrase);
             }
         }
 
